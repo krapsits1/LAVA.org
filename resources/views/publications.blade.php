b1b65cf5ebaa4474373187b8721a84cddfcc1706
Emils Vetra <emilsvetra@gmail.com>
2025-02-26 16:06:57 +0200

    test. publication view changed to fix 403 error

--- resources/views/publications.blade.php
+++ resources/views/publications.blade.php
@@ -12,7 +12,7 @@
     <div class="container mt-5">
         <h1 class="pt-4 pb-4 text-center">{{ __('messages.publications_title') }}</h1>
         <div class="d-flex justify-content-center">
-            <iframe src="{{ asset('publications/MedicusBonus_Raksts.pdf') }}" width="90%" height="600px"></iframe>
+            {{-- <iframe src="{{ asset('publications/MedicusBonus_Raksts.pdf') }}" width="90%" height="600px"></iframe> --}}
         </div>
     </div>
     
