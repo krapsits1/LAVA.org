16d60035cf7d129c71b1f956866239b1bdca75cb
Emils Vetra <emilsvetra@gmail.com>
2025-02-27 16:12:13 +0200

    publications

--- resources/views/publications.blade.php
+++ resources/views/publications.blade.php
@@ -9,10 +9,13 @@
 
 
 <body>
-    <div class="container mt-5">
+    <div class="container pt-5">
         <h1 class="pt-4 pb-4 text-center">{{ __('messages.publications_title') }}</h1>
         <div class="d-flex justify-content-center">
-            <iframe src="{{ asset('publications/MedicusBonus_Raksts.pdf') }}" width="90%" height="600px"></iframe>
+            <iframe src="{{ asset('publ/MedicusBonus_Raksts.pdf') }}" width="90%" height="600px"></iframe>
+        </div>
+        <div class="d-flex justify-content-center pt-5 pb-5">
+            <iframe src="{{ asset('publ/MB_73_katetri_001.pdf') }}" width="90%" height="600px"></iframe>
         </div>
     </div>
     
